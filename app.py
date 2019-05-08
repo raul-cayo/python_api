b877716dcb4636699d42da399ef1513112bc19f2
Raul Cayo <raul.sanchez.cayo@gmail.com>
2019-05-07 21:16:27 -0500

    Create a method find_all() for the models

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('JAWSDB_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["PROPAGATE_EXCEPTIONS"] = True
 app.secret_key = 'cayo'
 api = Api(app)
 
