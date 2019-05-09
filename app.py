7e2799722c009beb93caa151a4734e94544d1825
Raul Cayo <raul.sanchez.cayo@gmail.com>
2019-05-08 21:22:47 -0500

    Added a black list to logout users

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 # Own libraries
-from resources.user import UserRegister, User, UserLogin, TokenRefresh
+from resources.user import (
+    UserRegister,
+    User,
+    UserLogin,
+    TokenRefresh,
+    UserLogout
+    )
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from blacklist import BLACKLIST
 
 # Initializing the app whit a secret_key for JTW to encode
 # and the information of the database
@@ -14,6 +21,8 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('JAWSDB_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["JWT_BLACKLIST_ENABLED"] = True
+app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = ["access", "refresh"]
 app.secret_key = 'cayo'  # app.config["JWT_SECRET_KEY"]
 api = Api(app)
 
@@ -28,6 +37,13 @@ def add_claims_to_jwt(identity):
     if identity == 4:
         return {"is_admin": True}
     return {"is_admin": False}
+
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    # decrypted_token["identity"] would
+    # block the user not only the token
+    return decrypted_token["jti"] in BLACKLIST
 
 
 # Cuztomazing callbacks and responses
@@ -79,6 +95,7 @@ api.add_resource(ItemList, "/items")
 api.add_resource(UserRegister, "/register")
 api.add_resource(User, "/user/<int:user_id>")
 api.add_resource(UserLogin, "/login")
+api.add_resource(UserLogout, "/logout")
 api.add_resource(TokenRefresh, "/refresh")
 
 # Run the API with displaying errors on HTML for debugging
